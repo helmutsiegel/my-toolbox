41b61d9795d99aeb919d04e1d08a0bffa378c912
helmutsiegel <helmutsiegel@yahoo.com>
2024-08-05 13:30:46 +0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
 echo "alias pull='git pull'"  >> ~/.bashrc
 echo "alias push='git push'"  >> ~/.bashrc
 echo "alias pullstash='git stash && git pull && git stash apply --index'"  >> ~/.bashrc
+echo "alias codebashrc='code ~/.bashrc'"  >> ~/.bashrc
