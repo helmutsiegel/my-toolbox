d5bcd889b835a5cde4423f4388e7434ef588fe81
helmutsiegel <helmutsiegel@yahoo.com>
2024-08-08 16:15:23 +0300

    Update proxy.js

--- node-proxy/proxy.js
+++ node-proxy/proxy.js
@@ -4,9 +4,25 @@ const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
 const server = http.createServer((req, res) => {
+  // Log the original request
+  console.log('Original request:', {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+  });
+  
   // Append the custom header
   req.headers['X-Custom-Header'] = 'My Custom Value';
 
+ proxy.on('proxyReq', (proxyReq, _req, _res, options) => {
+    // Log the forwarded request
+    console.log('Forwarded request:', {
+      method: proxyReq.method,
+      url: proxyReq.path,
+      headers: proxyReq.getHeaders(),
+    });
+  });
+  
   proxy.web(req, res, {
     target: 'http://example.com', // Target server URL
     changeOrigin: true,
