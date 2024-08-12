fe94220179532ca5c37a402a0831f1e582f19dd9
Helmut Siegel <helmut.siegel.dev@gmail.com>
2024-08-12 15:15:10 +0300

    Fixed aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -5,7 +5,7 @@ echo "alias push='git push'"  >> ~/.bashrc
 echo "alias pullstash='git stash && git pull && git stash apply --index'"  >> ~/.bashrc
 echo "alias codebashrc='code ~/.bashrc'"  >> ~/.bashrc
 echo "alias loadbashrc='source ~/.bashrc'"  >> ~/.bashrc
-echo "alias status='git status"  >> ~/.bashrc
-echo "alias commit='git commit -am"  >> ~/.bashrc
+echo "alias status='git status'"  >> ~/.bashrc
+echo "alias commit='git commit -am'"  >> ~/.bashrc
 
 echo "# Helmut Tool-Box aliases END" >> ~/.bashrc
