0f7f3f39c5c629595ad272659c5dbeeddae2a346
helmutsiegel <helmutsiegel@yahoo.com>
2024-09-11 11:46:03 +0300

    Update aliases.sh

--- aliases/aliases.sh
+++ aliases/aliases.sh
@@ -1,14 +1,14 @@
 # Helmut Tool-Box aliases BEGIN
 
-alias pull="git pull"
-alias push="git push"
-alias pullstash="git stash && git pull && git stash apply --index"
-alias codebashrc="code ~/.bashrc"
-alias loadbashrc="source ~/.bashrc"
-alias status="git status"
-alias commit="git add . && git commit -m"
-alias idea="/opt/idea-*/bin/./idea"
-alias resetidea="rm -r ~/.java/.userPrefs/"
-alias executable="chmod +x"
+alias pull='echo "git pull" && git pull'
+alias push='echo "git push" git push'
+alias pullstash='git stash && git pull && git stash apply --index'
+alias codebashrc='echo "code ~/.bashrc" && code ~/.bashrc'
+alias loadbashrc='source ~/.bashrc'
+alias status='git status'
+alias commit='git add . && git commit -m'
+alias idea='/opt/idea-*/bin/./idea'
+alias resetidea='rm -r ~/.java/.userPrefs/'
+alias executable='chmod +x'
 
 # Helmut Tool-Box aliases END"
